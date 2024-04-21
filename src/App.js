4391ba1e0f3c8e8e4aeab47a13006a56a6a268c0
mahesh_sv <memaheshwaran@gmail.com>
2024-04-21 16:53:02 +0530

    test commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize: "2rem",
+        backgroundColor: "lightblue",
+      }}
+    >
+      Mahesh
+    </div>
+  );
+};
+
+export default App;
